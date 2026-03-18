77ab8a00bc9450c1a95bfe8c47fab2ae9015d1ca
Ubuntu <ubuntu@ip-172-31-11-17.ec2.internal>
2026-03-18 11:31:48 +0000

    java

---
+++ HelloServlet.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<html><body>");
+        out.println("<h1>Hello from Java WAR Application!</h1>");
+        out.println("<h2>Deployed successfully on Tomcat</h2>");
+        out.println("</body></html>");
+    }
+}
